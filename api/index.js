150a529cf739e4a0d36c9b712693b3be85670ef9
mukilanbalu <mukilanbalu@gmail.com>
2024-07-02 21:45:55 +0530

    img path chng

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use("/api/uploads/images", express.static(path.join('.output', 'uploads', 'images')))
+app.use("/api/uploads/images", express.static(path.join('output', 'static', 'uploads', 'images')))
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
