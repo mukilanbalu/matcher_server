d3a54fa981018630c2b416ae5265d630592967fe
mukilanbalu <mukilanbalu@gmail.com>
2024-07-02 21:30:41 +0530

    adding cors

--- api/index.js
+++ api/index.js
@@ -6,10 +6,11 @@ const connectDB = require("../DB/db")
 const formidableMiddleware = require('express-formidable');
 const path = require('path');
 const app = express();
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/uploads/images", express.static(path.join('uploads', 'images')))
 
